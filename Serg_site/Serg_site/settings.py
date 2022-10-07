494ef3b058648ae7495a2b68f97da4731f72c688
sergtwix <63192455+sergtwix@users.noreply.github.com>
2022-10-07 14:07:44 +0100

    3 docker

--- Serg_site/Serg_site/settings.py
+++ Serg_site/Serg_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-empdy8bfs-1)f__q_hh)0xw(n_2upes60im*=l(97o*zfhx0up
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
